3d18a8d48c403f4470507f288a54331f5ad687cb
dalhatmd <umarwalid4055@gmail.com>
2023-08-04 16:44:26 +0100

    Added new folder

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "calc.h"
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("Please enter all necessary inputs\n");
+	}
+	for (int i = 0; i != NULL; i++)
+	{
+		printf("%s \n",argv[i]);
+//		printf('\n');
+	}
+	return (0);
+}
