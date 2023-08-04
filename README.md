f7a772bc1bab2b54d2fadf7f79bad7723370ba3d
dalhatmd <umarwalid4055@gmail.com>
2023-08-04 17:17:40 +0100

    New push!

---
+++ README.md
@@ -0,0 +1,8 @@
+A simple command line calculator
+
+Usage:
+    compile the code using gcc and run
+    For example: ./calculator 1 + 1
+
+
+    Will add more features soon 
--- main.c
+++ main.c
@@ -1,14 +1,32 @@
 #include "calc.h"
 int main(int argc, char **argv)
 {
-	if (argc < 3)
+	int a, b;
+	char op;
+
+	if (argc < 4)
 	{
 		printf("Please enter all necessary inputs\n");
 	}
-	for (int i = 0; i != NULL; i++)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op = argv[2][0];
+
+	switch (op)
 	{
-		printf("%s \n",argv[i]);
-//		printf('\n');
+		case '+':
+			printf("%d\n", a + b);
+		case '-':
+			printf("%d\n", a - b);
+		case '*':
+			printf("%d\n", a * b);
+		case '/':
+			if (a == 0 || b == 0)
+				printf("Error: Cannot divide by 0\n");
+			else
+				printf("%d\n", a / b);
 	}
+
 	return (0);
 }
